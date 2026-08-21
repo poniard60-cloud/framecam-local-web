fb884e5d76ed65102930983ef5d37ed85661e04a
poniard60-cloud <poniard60@gmail.com>
2026-08-21 10:34:05 +0900

    Refresh cache for smooth pinch zoom

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'framecam-local-v1-pages-20260821-transparent-window-zoom-v2';
+const CACHE = 'framecam-local-v1-pages-20260821-smooth-pinch-v1';
 const ASSETS = [
   './',
   './index.html',
@@ -14,6 +14,7 @@ const ASSETS = [
   './app-6.js',
   './app-7.js',
   './app-8.js',
+  './app-9.js',
   './manifest.webmanifest'
 ];
 
